f6a71f0af5993ef03b5b612e05a58d2bde41913d
Roman Dronov <roman.dronov1@gmail.com>
2016-11-29 13:06:23 -0800

    first update

---
+++ Source.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s2;
+	int start;
+	char str[300];
+	gets_s(str);
+	cout << str << endl;
+	cout << strlen(str) << endl;
+	int word = 0;
+	//size of the massive for the coordinates of the word, i like cats
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == ' ') {
+			word++;
+		}
+	}
+	int *star_coor = new int[word + 1];
+	int *end_coor = new int[word + 1];
+	cout << "word" << word << endl;
+	int a = 0;
+	//looking for the ccordinates of the word
+	for (int i = 0; i < strlen(str)+2; i++) {
+		if (str[i] == ' ') {
+			end_coor[a] = i;
+			a++;
+		}
+		if (str[i] == '\0') {
+			end_coor[a] = i;
+			a++;
+			break;
+		}
+	}
+	cout << "a" << a << endl;
+	cout << "mass" << endl;
+	for (int i = 0; i < word + 1; i++) {
+		cout << end_coor[i] << " ";
+}
+// when the word start
+	star_coor[0] = 0;
+	for (int i = 1; i < word + 1; i++) {
+		for (int j = 0; j < word + 1; j++) {
+			star_coor[i] = end_coor[j] + 1;
+		}
+	}
+	cout << "mass2";
+	for (int i = 0; i < word + 1; i++) {
+		cout << star_coor << " ";
+	}
+//	cout << "number of words" << word << endl;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == ' ') {
+			start = i;
+			break;
+			}
+		//else cout<<"p";
+		//cout << "num" << i << endl;
+		}
+	/*for (int i = 0; i < start; i++) {
+		cout << str[i];
+	}*/
+	cout << endl;
+	delete star_coor;
+	delete end_coor;
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+//#include <iostream>
+//#include <conio.h>
+//#include <cstring>
+//#include <windows.h>
+//using namespace std;
+//
+//int main()
+//{
+//	SetConsoleCP(1251); 
+//	setlocale(LC_ALL, "Russian");
+//
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//
+//	char str[80];
+//	char token[80];
+//	int t, g;
+//	cout << "Enter string:\n";
+//	gets_s(str);
+//
+//	cout << "\n\Your entered: " << str << '\n';
+//	cout << "Lenght: " << strlen(str) << '\n';
+//	cout << "\n";
+//
+//	// Считываем лексему из строки.
+//	for (t = 0; ; t++)
+//	{
+//		for (g = 0; str[t] != ' ' && str[t]; g++, t++)
+//			token[g] = str[t];
+//		token[g] = '\0'; // Завершаем лексему нулевым символом.
+//
+//		cout << "\nWord: " << token << '\t';
+//		cout << "Lenght: " << strlen(token) << '\n';
+//		for (int i = 0; i < 80; i++) {
+//
+//		}
+//		if (!str[t]) break;
+//	}
+//	_kbhit();
+//	system("pause");
+//	return 0;
+//}
